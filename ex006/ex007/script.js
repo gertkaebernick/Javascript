411215016b517331d9341be4a71781c616d12a12
Gert Kaebernick <107575055+gertkaebernick@users.noreply.github.com>
2022-07-26 17:26:43 -0300

    a

---
+++ ex006/ex007/script.js
@@ -0,0 +1,6 @@
+function calcular() {
+    var com = document.getElementById('inicio')
+    var fim = document.getElementById('fim')
+    var pas = document.getElementById('passo')
+    for(var )
+}
