cdf174465eac8e5e6783ff99f5d19eceaca2ba55
Gert Kaebernick <107575055+gertkaebernick@users.noreply.github.com>
2022-08-01 11:43:40 -0300

    a

--- ex006/ex007/script.js
+++ ex006/ex007/script.js
@@ -2,5 +2,9 @@ function calcular() {
     var com = document.getElementById('inicio')
     var fim = document.getElementById('fim')
     var pas = document.getElementById('passo')
-    for(var )
+
+    do{
+        var c = com
+        c --
+    }while(c > 0)
 }
