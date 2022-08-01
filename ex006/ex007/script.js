f56df2ca981f0a9653ec294d0eeec91e5c716332
Gert Kaebernick <107575055+gertkaebernick@users.noreply.github.com>
2022-08-01 12:53:22 -0300

    a

--- ex006/ex007/script.js
+++ ex006/ex007/script.js
@@ -1,10 +1,33 @@
-function calcular() {
-    var com = document.getElementById('inicio')
-    var fim = document.getElementById('fim')
-    var pas = document.getElementById('passo')
+function contar() {
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
-    do{
-        var c = com
-        c --
-    }while(c > 0)
+    if(ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados')
+        res.innerHTML = `Impossivel contar`
+    }else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if(p <= 0){
+            window.alert('Passo invalido, considerando passo 1')
+            p = 1
+        }
+
+        if(i < f){
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c}\u{1F449}`
+            }
+            
+        }else {
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c}\u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
+    }
 }
